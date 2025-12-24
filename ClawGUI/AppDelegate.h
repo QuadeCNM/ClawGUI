094fa63334f4049e97ad56094f6942dbd33654b6
Jon Wade <jon_wade@apple.com>
2025-12-23 20:44:24 -0800

    Initial Commit

---
+++ ClawGUI/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ClawGUI
+//
+//  Created by Jon Wade on 12/23/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ClawGUI/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ClawGUI
+//
+//  Created by Jon Wade on 12/23/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
